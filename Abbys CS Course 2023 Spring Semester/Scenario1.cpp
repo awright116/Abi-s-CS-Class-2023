a03909cc44bdd38db456659f6e944b95f3e6ec26
abiga <abiga@DESKTOP-UBO29E4>
2023-01-15 18:02:28 -0500

    Add project files.

---
+++ Abbys CS Course 2023 Spring Semester/Scenario1.cpp
@@ -0,0 +1,369 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char personQuality = '3';
+    char popularTvshow = '3';
+    char favSnack = '3';
+    char favApp = '3';
+    char yourSidekick = '3';
+    char weekendPlans = '3';
+    char favHobby = '3';
+    char musicGenre = '3';
+    char favDrink = '3';
+    char vacationSpot = '3';
+
+    printf("What Fandom Do You Belong To?\n");
+    printf("Type the given options A-F only for each question.\n");
+    printf("\n");
+
+
+    printf("What is one quality you admire about your bestfriend?\n");
+    printf("\n");
+    printf("a. Humor\n");
+    printf("b. Honesty\n");
+    printf("c. Loyalty\n");
+    printf("d. Good listener\n");
+    printf("e. Confident\n");
+    printf("f. Empathy\n");
+
+    while (personQuality != 'a' && personQuality != 'A' && personQuality != 'b' && personQuality != 'B' && personQuality != 'c' && personQuality != 'C' && personQuality != 'd' && personQuality != 'D' && personQuality != 'e' && personQuality != 'E' && personQuality != 'f' && personQuality != 'F') {
+
+
+        scanf_s(" %c", &personQuality);
+        printf("\n");
+
+        if (personQuality == 'a' || personQuality == 'A') {
+        }
+        else if (personQuality == 'b' || personQuality == 'B') {
+        }
+        else if (personQuality == 'c' || personQuality == 'C') {
+        }
+        else if (personQuality == 'd' || personQuality == 'D') {
+        }
+        else if (personQuality == 'e' || personQuality == 'E') {
+        }
+        else if (personQuality == 'f' || personQuality == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+
+    }
+
+    printf("What TV series do you rewatch the most? \n");
+    printf("a. New Girl \n");
+    printf("b. Demon Slayer \n");
+    printf("c. Supernatural \n");
+    printf("d. Gilmore Girls \n");
+    printf("e. Extraordinary Attorney Woo\n");
+    printf("f. How to Get Away With Murder \n");
+
+    while (popularTvshow != 'a' && popularTvshow != 'A' && popularTvshow != 'b' && popularTvshow != 'B' && popularTvshow != 'c' && popularTvshow != 'C' && popularTvshow != 'd' && popularTvshow != 'D' && popularTvshow != 'e' && popularTvshow != 'E' && popularTvshow != 'f' && popularTvshow != 'F') {
+
+
+        scanf_s(" %c", &popularTvshow);
+        printf("\n");
+
+        if (popularTvshow == 'a' || popularTvshow == 'A') {
+        }
+        else if (popularTvshow == 'b' || popularTvshow == 'B') {
+        }
+        else if (popularTvshow == 'c' || popularTvshow == 'C') {
+        }
+        else if (popularTvshow == 'd' || popularTvshow == 'D') {
+        }
+        else if (popularTvshow == 'e' || popularTvshow == 'E') {
+        }
+        else if (popularTvshow == 'f' || popularTvshow == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+
+
+    printf("What is your favorite late night snack?\n");
+    printf("a. Cookies\n");
+    printf("b. Popcorn\n");
+    printf("c. Nachos\n");
+    printf("d. Cheese\n");
+    printf("e. Fruit\n");
+    printf("f. Pretzels\n");
+
+    while (favSnack != 'a' && favSnack != 'A' && favSnack != 'b' && favSnack != 'B' && favSnack != 'c' && favSnack != 'C' && favSnack != 'd' && favSnack != 'D' && favSnack != 'e' && favSnack != 'E' && favSnack != 'f' && favSnack != 'F') {
+
+        scanf_s(" %c", &favSnack);
+        printf("\n");
+
+        if (favSnack == 'a' || favSnack == 'A') {
+        }
+        else if (favSnack == 'b' || favSnack == 'B') {
+        }
+        else if (favSnack == 'c' || favSnack == 'C') {
+        }
+        else if (favSnack == 'd' || favSnack == 'D') {
+        }
+        else if (favSnack == 'e' || favSnack == 'E') {
+        }
+        else if (favSnack == 'f' || favSnack == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+
+    printf("What app do your use the most?\n");
+    printf("a. Twitter\n");
+    printf("b. Snapchat\n");
+    printf("c. Instagram\n");
+    printf("d. Youtube\n");
+    printf("e. Spotify\n");
+    printf("f. Tiktok\n");
+
+    while (favApp != 'a' && favApp != 'A' && favApp != 'b' && favApp != 'B' && favApp != 'c' && favApp != 'C' && favApp != 'd' && favApp != 'D' && favApp != 'e' && favApp != 'E' && favApp != 'f' && favApp != 'F') {
+
+
+        scanf_s(" %c", &favApp);
+        printf("\n");
+
+        if (favApp == 'a' || favApp == 'A') {
+        }
+        else if (favApp == 'b' || favApp == 'B') {
+        }
+        else if (favApp == 'c' || favApp == 'C') {
+        }
+        else if (favApp == 'd' || favApp == 'D') {
+        }
+        else if (favApp == 'e' || favApp == 'E') {
+        }
+        else if (favApp == 'f' || favApp == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+
+    printf("Who would you choose as a sidekick?\n");
+    printf("a. Beth Dutton\n");
+    printf("b. Naruto Uzumaki\n");
+    printf("c. Chewbacca\n");
+    printf("d. Wednesday Adams\n");
+    printf("e. Dr Strange\n");
+    printf("f. Merida\n");
+
+    while (yourSidekick != 'a' && yourSidekick != 'A' && yourSidekick != 'b' && yourSidekick != 'B' && yourSidekick != 'c' && yourSidekick != 'C' && yourSidekick != 'd' && yourSidekick != 'D' && yourSidekick != 'e' && yourSidekick != 'E' && yourSidekick != 'f' && yourSidekick != 'F') {
+
+
+        scanf_s(" %c", &yourSidekick);
+        printf("\n");
+
+        if (yourSidekick == 'a' || yourSidekick == 'A') {
+        }
+        else if (yourSidekick == 'b' || yourSidekick == 'B') {
+        }
+        else if (yourSidekick == 'c' || yourSidekick == 'C') {
+        }
+        else if (yourSidekick == 'd' || yourSidekick == 'D') {
+        }
+        else if (yourSidekick == 'e' || yourSidekick == 'E') {
+        }
+        else if (yourSidekick == 'f' || yourSidekick == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+
+
+    printf("What is your ideal weekend?\n");
+    printf("a. Beinge a TV show\n");
+    printf("b. Go out with friends\n");
+    printf("c. Sleep all day\n");
+    printf("d. Travel somewhere new\n");
+    printf("e. Dinner and movies with partner\n");
+    printf("f. Enjoy time with family\n");
+
+    while (weekendPlans != 'a' && weekendPlans != 'A' && weekendPlans != 'b' && weekendPlans != 'B' && weekendPlans != 'c' && weekendPlans != 'C' && weekendPlans != 'd' && weekendPlans != 'D' && weekendPlans != 'e' && weekendPlans != 'E' && weekendPlans != 'f' && weekendPlans != 'F') {
+
+
+        scanf_s(" %c", &weekendPlans);
+        printf("\n");
+
+        if (weekendPlans == 'a' || weekendPlans == 'A') {
+        }
+        else if (weekendPlans == 'b' || weekendPlans == 'B') {
+        }
+        else if (weekendPlans == 'c' || weekendPlans == 'C') {
+        }
+        else if (weekendPlans == 'd' || weekendPlans == 'D') {
+        }
+        else if (weekendPlans == 'e' || weekendPlans == 'E') {
+        }
+        else if (weekendPlans == 'f' || weekendPlans == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+    printf("What is your favorite hobby?\n");
+    printf("a. Watching TV/ Movies\n");
+    printf("b. Dancing \n");
+    printf("c. Gaming \n");
+    printf("d. Fitness \n");
+    printf("e. Cooking \n");
+    printf("f. Reading \n");
+
+    while (favHobby != 'a' && favHobby != 'A' && favHobby != 'b' && favHobby != 'B' && favHobby != 'c' && favHobby != 'C' && favHobby != 'd' && favHobby != 'D' && favHobby != 'e' && favHobby != 'E' && favHobby != 'f' && favHobby != 'F') {
+
+
+        scanf_s(" %c", &favHobby);
+        printf("\n");
+
+        if (favHobby == 'a' || favHobby == 'A') {
+        }
+        else if (favHobby == 'b' || favHobby == 'B') {
+        }
+        else if (favHobby == 'c' || favHobby == 'C') {
+        }
+        else if (favHobby == 'd' || favHobby == 'D') {
+        }
+        else if (favHobby == 'e' || favHobby == 'E') {
+        }
+        else if (favHobby == 'f' || favHobby == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+    printf("What is your favorite music genre?\n");
+    printf("a. Hip-Hop\n");
+    printf("b. Country \n");
+    printf("c. Kpop \n");
+    printf("d. Latin \n");
+    printf("e. Rock \n");
+    printf("f. Indie \n");
+
+    while (musicGenre != 'a' && musicGenre != 'A' && musicGenre != 'b' && musicGenre != 'B' && musicGenre != 'c' && musicGenre != 'C' && musicGenre != 'd' && musicGenre != 'D' && musicGenre != 'e' && musicGenre != 'E' && musicGenre != 'f' && musicGenre != 'F') {
+
+
+        scanf_s(" %c", &musicGenre);
+        printf("\n");
+
+        if (musicGenre == 'a' || musicGenre == 'A') {
+        }
+        else if (musicGenre == 'b' || musicGenre == 'B') {
+        }
+        else if (musicGenre == 'c' || musicGenre == 'C') {
+        }
+        else if (musicGenre == 'd' || musicGenre == 'D') {
+        }
+        else if (musicGenre == 'e' || musicGenre == 'E') {
+        }
+        else if (musicGenre == 'f' || musicGenre == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+    printf("What is your favorite drink choice option?\n");
+    printf("a. Water\n");
+    printf("b. Pepsi products\n");
+    printf("c. Boba Tea\n");
+    printf("d. Coffee\n");
+    printf("e. Coke Products\n");
+    printf("f. Tea\n");
+
+    while (favDrink != 'a' && favDrink != 'A' && favDrink != 'b' && favDrink != 'B' && favDrink != 'c' && favDrink != 'C' && favDrink != 'd' && favDrink != 'D' && favDrink != 'e' && favDrink != 'E' && favDrink != 'f' && favDrink != 'F') {
+
+
+        scanf_s(" %c", &favDrink);
+        printf("\n");
+
+        if (favDrink == 'a' || favDrink == 'A') {
+        }
+        else if (favDrink == 'b' || favDrink == 'B') {
+        }
+        else if (favDrink == 'c' || favDrink == 'C') {
+        }
+        else if (favDrink == 'd' || favDrink == 'D') {
+        }
+        else if (favDrink == 'e' || favDrink == 'E') {
+        }
+        else if (favDrink == 'f' || favDrink == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+    printf("What is your ideal vacation spot?\n");
+    printf("a. Paris\n");
+    printf("b. Japan\n");
+    printf("c. Rome\n");
+    printf("d. Bora Bora\n");
+    printf("e. Grand Canyon\n");
+    printf("f. Barcelona\n");
+
+    while (vacationSpot != 'a' && vacationSpot != 'A' && vacationSpot != 'b' && vacationSpot != 'B' && vacationSpot != 'c' && vacationSpot != 'C' && vacationSpot != 'd' && vacationSpot != 'D' && vacationSpot != 'e' && vacationSpot != 'E' && vacationSpot != 'f' && vacationSpot != 'F') {
+
+
+        scanf_s(" %c", &vacationSpot);
+        printf("\n");
+
+        if (vacationSpot == 'a' || vacationSpot == 'A') {
+        }
+        else if (vacationSpot == 'b' || vacationSpot == 'B') {
+        }
+        else if (vacationSpot == 'c' || vacationSpot == 'C') {
+        }
+        else if (vacationSpot == 'd' || vacationSpot == 'D') {
+        }
+        else if (vacationSpot == 'e' || vacationSpot == 'E') {
+        }
+        else if (vacationSpot == 'f' || vacationSpot == 'F') {
+        }
+        else
+            printf("Please enter an acceptable answer\n");
+    }
+
+    if (personQuality == 'e' || personQuality == 'E' && popularTvshow == 'e' || popularTvshow == 'E' && favSnack == 'c' || favSnack == 'C' && favApp == 'e' || favApp == 'E' && yourSidekick == 'f' || yourSidekick == 'F' && weekendPlans == 'b' || weekendPlans == 'B' && favHobby == 'b' || favHobby == 'B' && musicGenre == 'c' || musicGenre == 'C' && favDrink == 'c' || favDrink == 'C' && vacationSpot == 'd' || vacationSpot == 'D') {
+        printf("Your fandom reccomendation is: BlackPink KPOP Fandom\n");
+    }
+    else if (personQuality == 'a' || personQuality == 'A' && popularTvshow == 'a' || popularTvshow == 'A' && favSnack == 'a' || favSnack == 'A' && favApp == 'a' || favApp == 'A' && yourSidekick == 'a' || yourSidekick == 'A' && weekendPlans == 'a' || weekendPlans == 'A' && favHobby == 'a' || favHobby == 'A' && musicGenre == 'a' || musicGenre == 'A' && favDrink == 'a' || favDrink == 'A' && vacationSpot == 'a' || vacationSpot == 'A') {
+        printf("Unfortunanty your 10 choices did not match with any current fandoms.\n");
+        printf("Based on your responses our team recommends for you to create your own fandom.\n");
+        printf("Good luck!\n");
+    }
+        else if (personQuality == 'c' || personQuality == 'C' && popularTvshow == 'b' || popularTvshow == 'B' && favSnack == 'b' || favSnack == 'B' && favApp == 'a' || favApp == 'A' && yourSidekick == 'b' || yourSidekick == 'B' && weekendPlans == 'f' || weekendPlans == 'F' && favHobby == 'd' || favHobby == 'D' && musicGenre == 'e' || musicGenre == 'E' && favDrink == 'd' || favDrink == 'D' && vacationSpot == 'b' || vacationSpot == 'B') {
+        printf("Your fandom reccomendation is: Black Clover Anime Fandom\n");
+    }
+        else if (personQuality == 'd' || personQuality == 'D' && popularTvshow == 'f' || popularTvshow == 'F' && favSnack == 'b' || favSnack == 'B' && favApp == 'd' || favApp == 'D' && yourSidekick == 'a' || yourSidekick == 'A' && weekendPlans == 'f' || weekendPlans == 'F' && favHobby == 'f' || favHobby == 'F' && musicGenre == 'f' || musicGenre == 'F' && favDrink == 'f' || favDrink == 'F' && vacationSpot == 'c' || vacationSpot == 'C') {
+        printf("Your fandom reccomendation is: Harry Potter Fandom\n");
+    }
+        else if (personQuality == 'd' || personQuality == 'D' && popularTvshow == 'a' || popularTvshow == 'A' && favSnack == 'e' || favSnack == 'E' && favApp == 'c' || favApp == 'C' && yourSidekick == 'd' || yourSidekick == 'D' && weekendPlans == 'a' || weekendPlans == 'A' && favHobby == 'b' || favHobby == 'B' && musicGenre == 'c' || musicGenre == 'C' && favDrink == 'e' || favDrink == 'E' && vacationSpot == 'f' || vacationSpot == 'F') {
+        printf("Your fandom reccomendation is: Adults of Disney Fandom\n");
+    }
+        else if (personQuality == 'a' || personQuality == 'A' && popularTvshow == 'a' || popularTvshow == 'A' && favSnack == 'e' || favSnack == 'E' && favApp == 'c' || favApp == 'C' && yourSidekick == 'b' || yourSidekick == 'B' && weekendPlans == 'd' || weekendPlans == 'D' && favHobby == 'c' || favHobby == 'C' && musicGenre == 'c' || musicGenre == 'C' && favDrink == 'f' || favDrink == 'F' && vacationSpot == 'f' || vacationSpot == 'F') {
+        printf("Your fandom reccomendation is: Crazy Rich Asians Bookclub Fandom\n");
+    }
+        else if (personQuality == 'b' || personQuality == 'B' && popularTvshow == 'd' || popularTvshow == 'D' && favSnack == 'c' || favSnack == 'C' && favApp == 'a' || favApp == 'A' && yourSidekick == 'f' || yourSidekick == 'F' && weekendPlans == 'e' || weekendPlans == 'E' && favHobby == 'a' || favHobby == 'A' && musicGenre == 'a' || musicGenre == 'A' && favDrink == 'b' || favDrink == 'B' && vacationSpot == 'e' || vacationSpot == 'E') {
+        printf("Your fandom reccomendation is: Marvel Universe Fandom\n");
+    }
+        else if (personQuality == 'd' || personQuality == 'D' && popularTvshow == 'a' || popularTvshow == 'A' && favSnack == 'c' || favSnack == 'C' && favApp == 'c' || favApp == 'C' && yourSidekick == 'a' || yourSidekick == 'A' && weekendPlans == 'a' || weekendPlans == 'A' && favHobby == 'c' || favHobby == 'C' && musicGenre == 'c' || musicGenre == 'C' && favDrink == 'd' || favDrink == 'D' && vacationSpot == 'b' || vacationSpot == 'B') {
+        printf("Your fandom reccomendation is: Game of Thrones Fandom\n");
+    }
+        else if (personQuality == 'e' || personQuality == 'E' && popularTvshow == 'e' || popularTvshow == 'E' && favSnack == 'd' || favSnack == 'D' && favApp == 'c' || favApp == 'C' && yourSidekick == 'f' || yourSidekick == 'F' && weekendPlans == 'b' || weekendPlans == 'B' && favHobby == 'b' || favHobby == 'B' && musicGenre == 'b' || musicGenre == 'B' && favDrink == 'f' || favDrink == 'F' && vacationSpot == 'f' || vacationSpot == 'F') {
+        printf("Your fandom reccomendation is: Star Wars Fandom\n");
+    }
+        else if (personQuality == 'b' || personQuality == 'B' && popularTvshow == 'a' || popularTvshow == 'A' && favSnack == 'a' || favSnack == 'A' && favApp == 'c' || favApp == 'C' && yourSidekick == 'f' || yourSidekick == 'F' && weekendPlans == 'e' || weekendPlans == 'E' && favHobby == 'a' || favHobby == 'A' && musicGenre == 'a' || musicGenre == 'A' && favDrink == 'a' || favDrink == 'A' && vacationSpot == 'f' || vacationSpot == 'F') {
+        printf("Your fandom reccomendation is: Philly Eagles NFL Fandom\n");
+    }
+        else if (personQuality == 'c' || personQuality == 'C' && popularTvshow == 'b' || popularTvshow == 'B' && favSnack == 'b' || favSnack == 'B' && favApp == 'a' || favApp == 'A' && yourSidekick == 'b' || yourSidekick == 'B' && weekendPlans == 'c' || weekendPlans == 'C' && favHobby == 'd' || favHobby == 'D' && musicGenre == 'a' || musicGenre == 'A' && favDrink == 'd' || favDrink == 'D' && vacationSpot == 'b' || vacationSpot == 'B') {
+        printf("Your fandom reccomendation is: Pacers NBA Fandom\n");
+    }
+        
+    return 0;
+}
+
+
